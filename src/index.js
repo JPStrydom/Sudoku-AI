d72e19200de282072fecc4393031104f4b96793f
JP Strydom <jp.strydom@entelect.co.za>
2017-09-18 15:58:29 +0200

    Add redux dev tool support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+
 
 import App from './components/app';
 import reducers from './redux/reducers';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, composeEnhancers())}>
         <App />
     </Provider>,
     document.querySelector('.container')
