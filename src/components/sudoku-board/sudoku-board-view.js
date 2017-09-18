48030685b1c2e2bc08da250ae6b709fbd874fcc0
JP Strydom <jpstrydom8@gmail.com>
2017-09-19 01:32:32 +0200

    Add redux thunk refactor to view

--- src/components/sudoku-board/sudoku-board-view.js
+++ src/components/sudoku-board/sudoku-board-view.js
@@ -4,24 +4,7 @@ export default class SudokuBoardView extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            solvedCells: [
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false],
-                [false, false, false, false, false, false, false, false, false]
-            ],
-            solved: false
-        };
-
         this.renderSudokuBoard = this.renderSudokuBoard.bind(this);
-        this.solve = this.solve.bind(this);
-        this.reset = this.reset.bind(this);
         this.getCellValue = this.getCellValue.bind(this);
         this.handleCellInput = this.handleCellInput.bind(this);
         this.getCellStyling = this.getCellStyling.bind(this);
@@ -34,12 +17,12 @@ export default class SudokuBoardView extends Component {
                 {this.renderSudokuBoard()}
                 <button
                     className={'button'}
-                    onClick={this.solve}
-                    disabled={!this.props.sudokuBoard.isValid || this.state.solved}
+                    onClick={this.props.solveBoard}
+                    disabled={!this.props.sudokuBoard.isValid || this.props.sudokuBoard.isSolved}
                 >
                     <span>Solve</span>
                 </button>
-                <button className={'button'} onClick={this.reset}>
+                <button className={'button'} onClick={this.props.reset}>
                     <span>Reset</span>
                 </button>
             </div>
@@ -62,7 +45,7 @@ export default class SudokuBoardView extends Component {
                                         value={this.getCellValue(rowIndex, colIndex)}
                                         onChange={event => this.handleCellInput(event, rowIndex, colIndex)}
                                         className={this.getCellStyling(rowIndex, colIndex)}
-                                        disabled={this.state.solved}
+                                        disabled={this.props.sudokuBoard.isSolved}
                                     />
                                 </td>
                             ))}
@@ -73,45 +56,32 @@ export default class SudokuBoardView extends Component {
         );
     }
 
-    solve() {
-        const board = this.props.sudokuBoard.board;
-        const solvedCells = board.map(row => row.map(cell => !(cell >= 1 && cell <= 9)));
-        this.setState({ solvedCells, solved: true });
-        this.props.solveBoard(board);
-    }
-
-    reset() {
-        this.setState({ solved: false });
-        this.props.resetBoard();
-    }
-
     getCellValue(row, col) {
         const board = this.props.sudokuBoard.board;
-        const value = Math.abs(board[row][col]);
+        const value = board[row][col];
         return value >= 1 && value <= 9 ? value : '';
     }
 
     getCellStyling(row, col) {
         const board = this.props.sudokuBoard.board;
-        if (this.state.solvedCells[row][col] && this.state.solved) {
+        if (this.props.sudokuBoard.solvedCells[row][col] && this.props.sudokuBoard.isSolved) {
             return 'solved-cell';
         }
-        if (board[row][col] >= 1 && board[row][col] <= 9) {
-            return 'valid-cell';
+        if (this.props.sudokuBoard.errorCells[row][col]) {
+            return 'invalid-cell';
         }
         if (board[row][col] === 0) {
             return null;
         }
-        return 'invalid-cell';
+        return 'valid-cell';
     }
 
     handleCellInput(event, row, col) {
-        const board = this.props.sudokuBoard.board;
         let value = event.target.value[event.target.value.length - 1];
-        this.props.updateCell(row, col, value, board);
+        this.props.updateCell(row, col, value);
         if (!value) {
-            return this.props.clearBoardErrors(board);
+            return this.props.clearBoardErrors();
         }
-        setTimeout(() => this.props.clearBoardErrors(board), 500);
+        setTimeout(() => this.props.clearBoardErrors(), 500);
     }
 }
