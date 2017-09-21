878eb3f3c2f2762749e3ea1c527adc04c9bb88d1
JP Strydom <jpstrydom8@gmail.com>
2017-09-22 01:57:17 +0200

    Add temporary fix to generator

--- src/components/sudoku-board/utilities/sudoku-board-generator.js
+++ src/components/sudoku-board/utilities/sudoku-board-generator.js
@@ -1,7 +1,17 @@
 import { validateCell } from '../utilities/sudoku-board-validator';
+import { hasUniqueSolution } from '../utilities/sudoku-AI';
 
 function getRandomIntBetween(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function findEmptyCell(board) {
+    let row, col;
+    do {
+        row = getRandomIntBetween(0, 8);
+        col = getRandomIntBetween(0, 8);
+    } while (board[row][col] !== 0);
+    return { row, col };
 }
 
 export default function generateSudokuBoard() {
@@ -16,18 +26,27 @@ export default function generateSudokuBoard() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
-    let row , col;
+    let cell;
+    let row, col;
 
-    const initialPopulatedCellsCount = getRandomIntBetween(18, 36);
+    const initialPopulatedCellsCount = getRandomIntBetween(16, 32);
+    console.log('Initial Populated Cells Count: ', initialPopulatedCellsCount);
     for (let i = 0; i < initialPopulatedCellsCount; i++) {
-        row = getRandomIntBetween(0, 8);
-        col = getRandomIntBetween(0, 8);
-        board[row][col] = getRandomIntBetween(1, 9);
-        while (!validateCell(row, col, board)) {
-            row = getRandomIntBetween(0, 8);
-            col = getRandomIntBetween(0, 8);
+        do {
+            cell = findEmptyCell(board);
+            row = cell.row;
+            col = cell.col;
             board[row][col] = getRandomIntBetween(1, 9);
-        }
+        } while (!validateCell(row, col, board));
     }
     return board;
+    /*while (!hasUniqueSolution(board)) {
+        do {
+            cell = findEmptyCell(board);
+            row = cell.row;
+            col = cell.col;
+            board[row][col] = getRandomIntBetween(1, 9);
+        } while (!validateCell(row, col, board));
+    }
+    return board;*/
 }
