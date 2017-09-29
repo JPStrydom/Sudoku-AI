f9b86d34e3fb31f5335e9f9d16f5ab2e19489edf
JP Strydom <jpstrydom8@gmail.com>
2017-09-29 23:09:47 +0200

    Add cute console welcome message

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,17 @@ import { Component } from 'react';
 
 import SudokuBoard from './sudoku-board/sudoku-board-container';
 
+const consoleWelcomeMessage = '\n\n __          __  _                            _           _____           _       _                     _____ \n' +
+    ' \\ \\        / / | |                          | |         / ____|         | |     | |              /\\   |_   _|\n' +
+    '  \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___  | |_ ___   | (___  _   _  __| | ___ | | ___   _     /  \\    | |  \n' +
+    '   \\ \\/  \\/ / _ \\ |/ __/ _ \\| \'_ ` _ \\ / _ \\ | __/ _ \\   \\___ \\| | | |/ _` |/ _ \\| |/ / | | |   / /\\ \\   | |  \n' +
+    '    \\  /\\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |  ____) | |_| | (_| | (_) |   <| |_| |  / ____ \\ _| |_ \n' +
+    '     \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/  |_____/ \\__,_|\\__,_|\\___/|_|\\_\\\\__,_| /_/    \\_\\_____|\n' +
+    '                                                                                                              \n\n';
+
 export default class App extends Component {
     render() {
+        console.log(consoleWelcomeMessage);
         return (
             <div>
                 <SudokuBoard />
