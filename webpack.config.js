974bebd9537e2b9524c9e81ff44110703d9e2251
JP Strydom <jpstrydom8@gmail.com>
2017-09-30 02:08:37 +0200

    Added prettier to precommit

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  entry: ["./src/index.js"],
-  output: {
-    path: __dirname,
-    publicPath: "/",
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: "babel"
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  devServer: {
-    contentBase: "./"
-  }
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './'
+    }
 };
